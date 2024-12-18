eb45152814efee069c66e5b0d041610ba8787527
Daniel Barroso <dabarr02@ucm.es>
2024-12-18 15:41:02 +0100

    Comentado EJ1 PR3

--- Practica_3/ficheros_p3/ejercicio1/copy.c
+++ Practica_3/ficheros_p3/ejercicio1/copy.c
@@ -8,29 +8,33 @@ void copy(int fdo, int fdd)
 {
 	int nread;
 	char buff[BUFSIZE];
-	while((nread = read(fdo,buff,BUFSIZE))>0){
-		if (write(fdd, buff, nread) < nread) {
+	while((nread = read(fdo,buff,BUFSIZE))>0){ //Leemos en bloques de 512, almacenando en buff. El bucle sigue mientras no de error (-1) y no lleguemos a EOF (0)
+		if (write(fdd, buff, nread) < nread) {//Escribimos lo leido. Da error si el tamaño escrito (ret de write) es menor que lo que debe (nread) 
 			perror("write");
 			close(fdd); close(fdo);
-			exit(1);
+			exit(EXIT_FAILURE);
 		}		
 	}
-	if (nread < 0) {
+	if (nread < 0) { //Error al leer
 		perror("read");
 		close(fdd); close(fdo);
-		exit(1);
+		exit(EXIT_FAILURE);
 	}
 	close(fdd); close(fdo);
-	exit(0);
+	exit(EXIT_SUCCESS);
 
 }
 
 int main(int argc, char *argv[])
 {
-	int fdd =creat(argv[2],0644);
+	if (argc != 3) {
+        fprintf(stderr, "Uso: %s <nombre_archivo_origen> <nombre_archivo_destino>\n", argv[0]);
+        exit(EXIT_FAILURE);
+    }
+	int fdd =creat(argv[2],0644); //creamos archivo destino
 	if(fdd==-1)
 		exit(EXIT_FAILURE);
-	int fdo = open (argv[1],O_RDONLY);
+	int fdo = open (argv[1],O_RDONLY); //abrimos archivo origen
 	if(fdo==-1)
 		exit(EXIT_FAILURE);
 	
